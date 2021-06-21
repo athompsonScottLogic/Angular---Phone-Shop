640f5f27ddb05ed37507acd9eca1880c086f3ed1
athompsonScottLogic <athompson@scottlogic.com>
2021-06-21 11:48:44 +0100

    Share and Notify me buttons and functionality added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  templateUrl: 'product-alerts.component.html',
+  styleUrls: ['product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
